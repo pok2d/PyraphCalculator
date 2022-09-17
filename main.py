6d5549947570a63353f58675d5bc3de8e27ac1c2
pok2d <113487242+pok2d@users.noreply.github.com>
2022-09-17 18:01:25 -0400

    Optimization + Explanation/Notes + more
    Optimized like entire program, put a buncha # notes to clarify like every part of the program.
    The line's center/starting point is now in the center of the window.
    Overall entire thing is like 10x better now.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+
 blackpix = pygame.image.load('singularblackpixel.png')
 
 slope = int(input('Please put your slope (integer): '))
@@ -11,31 +12,42 @@ window = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption('Pyraph Graphing Calculator')
 running = True
 
+# The two lists for where we will be saving each x value with its y value for loading.
 xvalueslist = []
 yvalueslist = []
 
-timesslope = 1
 
-printinglinestartnum = 0
-
-for xvalue in range(0, 1000):
-    yvalue = timesslope * slope * -1
-    yvalue -= bvalue - 1000
-    timesslope += 1
+# Main calculation for slope and b value.
+for xvalue in range(-500, 500):
+    # This calculates slope, and the reason we have a * -1 is for pygame basically counting every pixel down or right as
+    # positive, completely opposite from how we normally graph it. So, we need the slope reversed.
+    yvalue = xvalue * slope * -1
+    # Here you can see we add 500 to our x and y value. This to center our line to our window. When we run this, our
+    # line starts are 0, 0, the top left corner of the screen. We want it to start in the center, so by adding 500 to
+    # both the default x and y values, it goes down (pos y)) to the verical center, and then right (pos x) to the
+    # horizontal center.
+    yvalue -= bvalue - 500
+    xvalue += 500
+    # We now simply add our xvalues and yvalues to 2 lists, for loading later.
     xvalueslist.append(xvalue)
     yvalueslist.append(yvalue)
 
+
+# Main game loop. If running == false, it all ends.
 while running:
+    # Detection if you press on the x button in the window.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # Creates white background.
     window.fill((255, 255, 255))
-    for item in xvalueslist:
-        window.blit(blackpix, (item, yvalueslist[printinglinestartnum]))
-        print(f'{item} and {yvalueslist[printinglinestartnum]}')
-        printinglinestartnum += 1
-    printinglinestartnum *= 0
-    window.convert_alpha()
+    # This is the loop for loading all our pixels for the line. It starts by taking an x value and then loading it with
+    # the correct y value.
+    for selectedxvalue in xvalueslist:
+        # Loading command.
+        window.blit(blackpix, (selectedxvalue, yvalueslist[selectedxvalue]))
+        # Next line can simply print current x and y value. Really only helpful for when troubleshooting program.
+        # print(f'{selectedxvalue} and {yvalueslist[selectedxvalue]}')
+
+    # Updates the window visually.
     pygame.display.update()
-
-
